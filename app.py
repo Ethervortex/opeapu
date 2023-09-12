89603d91df6991404bd04619a3a064c1cc323d72
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2023-09-12 20:45:56 +0300

    Use templates and PostgreSQL

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///teeruoko"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return "Home page"
+    return render_template("index.html")
