d4c2c9d3ee9c30bb3e0d6f33aac74a3de8aec63d
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2023-09-14 12:50:56 +0300

    Create test_students.sql

---
+++ test_students.sql
@@ -0,0 +1,19 @@
+INSERT INTO students (name) VALUES ('Jonni Myyrä');
+
+INSERT INTO students (name) VALUES ('Satu Mäkelä-Nummela');
+
+INSERT INTO students (name) VALUES ('Kalle Jalkanen');
+
+INSERT INTO students (name) VALUES ('Siiri Rantanen');
+
+INSERT INTO students (name) VALUES ('Tapio Rautavaara');
+
+INSERT INTO students (name) VALUES ('Marjut Rolig');
+
+INSERT INTO students (name) VALUES ('Jouko Salomäki');
+
+INSERT INTO students (name) VALUES ('Helena Takalo');
+
+INSERT INTO students (name) VALUES ('Kaarlo Kangasniemi');
+
+INSERT INTO students (name) VALUES ('Sirkka Polkunen');
