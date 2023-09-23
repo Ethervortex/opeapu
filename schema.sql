75d18226c25bfc93c8cf842b4cfada13221eaac4
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2023-09-23 12:09:00 +0300

    Update table schema

--- schema.sql
+++ schema.sql
@@ -14,10 +14,17 @@ CREATE TABLE courses (
     name TEXT
 );
 
+CREATE TABLE activity (
+    course_id INTEGER REFERENCES courses(id),
+    student_id INTEGER REFERENCES students(id),
+    PRIMARY KEY (course_id, student_id),
+    activity_score INTEGER,
+    activity_date TEXT
+);
+
 CREATE TABLE course_students (
     course_id INTEGER REFERENCES courses(id),
     student_id INTEGER REFERENCES students(id),
     PRIMARY KEY (course_id, student_id),
     grade INTEGER
 );
-
